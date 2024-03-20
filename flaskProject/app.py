0c24b9c79fba28b1fa9ebd02ac40997aa6a5a9df
HEEKWON <higun295@gmail.com>
2024-03-21 08:22:51 +0900

    doing flask server

--- flaskProject/app.py
+++ flaskProject/app.py
@@ -1,9 +1,13 @@
-from flask import Flask, jsonify
+from flask import Flask, jsonify, render_template
 from flask_cors import CORS
 import psutil
 
 app = Flask(__name__)
 CORS(app)
+
+@app.route('/')
+def home():
+    return render_template('index.html')
 
 @app.route('/metrics')
 def get_metrics():
