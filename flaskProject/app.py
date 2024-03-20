0563e4ca3f145b402fe8ec614e101105eb382840
HEEKWON <higun295@gmail.com>
2024-03-21 08:13:32 +0900

    first commit

---
+++ flaskProject/app.py
@@ -0,0 +1,15 @@
+from flask import Flask, jsonify
+from flask_cors import CORS
+import psutil
+
+app = Flask(__name__)
+CORS(app)
+
+@app.route('/metrics')
+def get_metrics():
+    cpu_usage = psutil.cpu_percent(interval=1)
+    memory_usage = psutil.virtual_memory().percent
+    return jsonify(cpu_usage=cpu_usage, memory_usage=memory_usage)
+
+if __name__ == '__main__':
+    app.run('127.0.0.1', port=5000)
